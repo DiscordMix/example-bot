c57c5f48de66a2b84a32887620d8b19af7368dde
CloudRex <cloudrex@outlook.com>
2018-12-24 22:52:29 -0500

    Implement latest version of Forge

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 // Environment Variables (defined in .env)
 require("dotenv").config();
 
-import {Bot, Settings} from "@cloudrex/forge";
 import path from "path";
 import fs from "fs";
+import {StoreActionType} from "./store";
+import {Bot, Settings, Command, CommandContext, Log, IStoreAction} from "@cloudrex/forge";
+import {EBotEvents} from "@cloudrex/forge/dist/core/bot";
+
+export interface ICommandLog {
+    readonly command: string;
+    readonly user: string;
+    readonly time: number;
+}
+
+export interface IState {
+    readonly commandLogs: ICommandLog[];
+}
 
 // Verify that .env file exists (bot configuration)
 if (!fs.existsSync(".env")) {
@@ -12,7 +24,7 @@ if (!fs.existsSync(".env")) {
 }
 
 async function init(): Promise<void> {
-    const bot: Bot = new Bot({
+    const bot: Bot = new Bot<IState, StoreActionType>({
         settings: new Settings({
             general: {
                 prefixes: [process.env.PREFIX] as string[],
@@ -30,6 +42,35 @@ async function init(): Promise<void> {
 
     // Connect and start the bot
     await bot.connect();
+
+    // BONUS: Log commands
+    bot.store.addReducer((action: IStoreAction<any>, state?: IState): IState | null => {
+        if (action.type === StoreActionType.LogCommand) {
+            if (state === undefined) {
+                return {
+                    commandLogs: [action.payload]
+                };
+            }
+            else {
+                return {
+                    ...state,
+                    commandLogs: [...state.commandLogs, action.payload]
+                };
+            }
+        }
+
+        return null;
+    });
+
+    bot.on(EBotEvents.CommandExecuted, (command: Command, context: CommandContext) => {
+        bot.store.dispatch(StoreActionType.LogCommand, {
+            command: command.meta.name,
+            user: context.sender.tag,
+            time: Date.now()
+        });
+
+        Log.info(`Command '${command.meta.name}' was executed by '${context.sender.tag}'`);
+    });
 }
 
 init();
