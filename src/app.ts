dacf1a8fd33544c4be9549435c946e47da6af7b9
CloudRex <cloudrex@outlook.com>
2019-01-03 04:23:19 -0500

    Do not use async function

--- src/app.ts
+++ src/app.ts
@@ -13,39 +13,35 @@ if (!fs.existsSync(".env")) {
     process.exit(0);
 }
 
-async function init(): Promise<void> {
-    const bot: Bot = new Bot<IState, StoreActionType>({
-        settings: new Settings({
-            general: {
-                prefixes: [process.env.PREFIX] as string[],
-                token: process.env.TOKEN as string,
-            },
+const bot: Bot = new Bot<IState, StoreActionType>({
+    settings: new Settings({
+        general: {
+            prefixes: [process.env.PREFIX] as string[],
+            token: process.env.TOKEN as string,
+        },
 
-            paths: {
-                commands: path.join(__dirname, process.env.COMMANDS_DIR as string),
-                services: path.join(__dirname, process.env.SERVICES_DIR as string)
-            }
-        }),
+        paths: {
+            commands: path.join(__dirname, process.env.COMMANDS_DIR as string),
+            services: path.join(__dirname, process.env.SERVICES_DIR as string)
+        }
+    }),
 
-        owner: process.env.OWNER_ID
+    owner: process.env.OWNER_ID
+});
+
+// BONUS: Log commands using the store
+bot.store.addReducer(LogReducer);
+
+// Dispatch log event upon command execution
+bot.on(EBotEvents.CommandExecuted, (command: Command, context: Context) => {
+    bot.store.dispatch(StoreActionType.LogCommand, {
+        command: command.meta.name,
+        user: context.sender.tag,
+        time: Date.now()
     });
 
-    // Connect and start the bot
-    await bot.connect();
+    Log.info(`Command '${command.meta.name}' was executed by '${context.sender.tag}'`);
+});
 
-    // BONUS: Log commands using the store
-    bot.store.addReducer(LogReducer);
-
-    // Dispatch log event upon command execution
-    bot.on(EBotEvents.CommandExecuted, (command: Command, context: Context) => {
-        bot.store.dispatch(StoreActionType.LogCommand, {
-            command: command.meta.name,
-            user: context.sender.tag,
-            time: Date.now()
-        });
-
-        Log.info(`Command '${command.meta.name}' was executed by '${context.sender.tag}'`);
-    });
-}
-
-init();
+// Connect and start the bot
+bot.connect();
