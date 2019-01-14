6fa1bbad8b93152523082c98addf443e1be01cd2
CloudRex <cloudrex@outlook.com>
2019-01-14 13:26:58 -0500

    Rename constraints

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,20 +1,11 @@
-import {Command, Context} from "@cloudrex/forge";
+import {Command, Context, Name, Description, Constraint} from "@cloudrex/forge";
 
+@Name("hello")
+@Description("Say hello world")
+@Constraint.Cooldown(3) // Must wait 3 seconds between executions (per-user)
 export default class HelloCommand extends Command {
-    // Basic information about this command
-    readonly meta = {
-        name: "hello",
-        description: "Say hello world"
-    };
-
-    readonly constraints: any = {
-        cooldown: 3 // Must wait 3 seconds between executions (per-user)
-    };
-
     public async run(context: Context): Promise<void> {
         await context.ok("Hello world"); // Success embed
         await context.fail("Hello world"); // Failure embed (auto-deletes; time depends on the message length)
-
-        // ... and some more which I will document in the future
     }
 };
